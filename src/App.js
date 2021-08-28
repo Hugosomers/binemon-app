b40ab60b1c6037e85783503e42e6688b558b040b
Hugosomers <hugosomersla@gmail.com>
2021-08-28 12:28:16 -0300

    Start Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [data, setData] = useState(false);
+
+  console.log(data);
+
+  const inputHandle = ({target}) => {
+    setInputValue(target.value);
+  }
+
+  const dataFetch = () => {
+    fetch(`https://api.binemon.io/api/binemons/token/${inputValue}`)
+    .then((response) => response.json())
+    .then((monInfo) => setData(monInfo.response))
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1 className="h1Title">Binemon Skills Search</h1>
+
+      <div className="searchDiv">
+        <form>
+          <h3>Digite um ID de MON:</h3>
+          <input 
+            type="text" 
+            value={inputValue}
+            onChange={inputHandle}
+          />
+          <button 
+            type="button"
+            onClick={dataFetch}
+          >
+            Buscar MON
+          </button>
+        </form>
+      </div>
+
+      {data &&
+        <div className="binemonDiv">
+          <div className="monApresentation">
+            <h1>{data.binemon_item.name}</h1>
+            <img src={data.binemon_item.thumbnail} alt="binemon" className="binemonImg"/>
+          </div>
+          <h3>Classe: {data.binemon_item.class === 3 ? 'Suporte' : null}</h3>
+          <div className="skillsDiv">
+            <h2>Skills:</h2>
+            <ul className="skillsList">
+              {data.binemon_item.skills && data.binemon_item.skills.map((item) => <li className="skillsItem">{item}</li>)}
+            </ul>
+          </div>
+        </div>
+      }
+
     </div>
   );
 }
