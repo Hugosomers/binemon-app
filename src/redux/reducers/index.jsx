7be54702fdbe3578f25c67f490f75db6a9a8b64d
Hugosomers <hugosomersla@gmail.com>
2021-09-07 18:44:53 -0300

    Update project

---
+++ src/redux/reducers/index.jsx
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import binemonReducer from "./binemonReducer";
+
+const rootReducer = combineReducers({
+  binemonReducer,
+})
+
+export default rootReducer;
